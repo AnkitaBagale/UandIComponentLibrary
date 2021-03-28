1c51b408b34521969af773c1fd6623d66b6d8331
AnkitaBagale <manali.bagale@gmail.com>
2021-03-28 11:42:34 +0530

    fix: review comments

--- Docsite-js/docsite-sidebar.js
+++ Docsite-js/docsite-sidebar.js
@@ -1,12 +1,11 @@
 const mySideBar = document.querySelector("#my-side-bar");
 const sideBarButton = document.querySelector(".nav-bar-opening-button");
 const sideBarNavLinks = document.querySelectorAll(".nav-link-item");
-    
-sideBarButton.addEventListener("click", ()=>{
-    mySideBar.classList.toggle("sidebar-active");
-})
-sideBarNavLinks.forEach((link)=>{
-    link.addEventListener("click",()=>{
-        mySideBar.classList.toggle("sidebar-active"); 
+
+const arrayOfClickableItemsToToggleSideBar = [sideBarButton, ...sideBarNavLinks]
+
+arrayOfClickableItemsToToggleSideBar.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        mySideBar.classList.toggle("sidebar-active");
     })
 })
