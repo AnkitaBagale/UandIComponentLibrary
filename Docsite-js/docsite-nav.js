492a199b0151915b21f728cb8a8df35a481318ac
ankitabagale <manali.bagale@gmail.com>
2021-06-17 10:37:02 +0530

    fix: small UI issues

--- Docsite-js/docsite-nav.js
+++ Docsite-js/docsite-nav.js
@@ -1,7 +1,7 @@
 //nav-bar
-const myNav = document.querySelector("#my-nav-bar");
-const myBurger = document.querySelector("#my-burger-menu");
+const myNav = document.querySelector('#my-nav-bar');
+const myBurger = document.querySelector('#my-burger-menu');
 
-myBurger.addEventListener("click", ()=>{
-    myNav.classList.toggle("active");
-})
+myBurger.addEventListener('click', () => {
+	myNav.classList.toggle('active');
+});
